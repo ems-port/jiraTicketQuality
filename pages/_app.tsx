bb0ec9a3a88bc97b46f002397054f2b0dddc1dda
ES-PORT <eryk@port.app>
2025-10-24 14:02:06 +0100

    Added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
 import "@/styles/globals.css";
 
+const Analytics = dynamic(
+  () => import("@vercel/analytics/react").then((mod) => mod.Analytics),
+  { ssr: false }
+);
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
